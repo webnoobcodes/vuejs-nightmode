52a348497cee32eb29ac2f0b1330e1156f0bc1f1
Mario <support@webnoobcodes.com>
2020-09-22 07:32:50 +0200

    initial

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-# vuejs-nightmode
+# Dark Layout Mode with Vue JS
+
+That's the complete source code with comments from the video tutorial [Dark Layout Mode with Vue JS](https://youtu.be/NzF2y673Gxs)
+
+A Dark Layout Mode implemented with Vue JS. The state is persisted in local storage from the browser. I use font awesome icons for the dark mode toggle.
+
+[![Tutorial on Youtube](http://img.youtube.com/vi/NzF2y673Gxs/0.jpg)](https://youtu.be/NzF2y673Gxs)
+
+## 📺 Complete Playlists
+
+[Learn Vue JS - From The Ground Up](https://www.youtube.com/playlist?list=PLINmvGGUwYc3fsN0ba_ZSZE0ywDeS0cd2)
+
+[Learn Vue JS - Additional Libaries](https://www.youtube.com/playlist?list=PLINmvGGUwYc0KrI3N11BCzflF6lEXqR9M)
+
+[Best Vue JS Tips and Tricks](https://www.youtube.com/playlist?list=PLINmvGGUwYc1GHkWFayyTLJXlnssHQEPc)
+
+[Vue JS Animations and Transitions](https://www.youtube.com/playlist?list=PLINmvGGUwYc1qRSE4LMjReEULDE1pa-mS)
+
+[Learn Nuxt JS - From The Ground Up](https://www.youtube.com/playlist?list=PLINmvGGUwYc11ZniENQRU7eQar6gQo04R)
+
+## Connect with me:
+
+[<img align="left" alt="weboob.dev" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" />][website]
+[<img align="left" alt="webnoob | YouTube" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/youtube.svg" />][youtube]
+[<img align="left" alt="webnoob | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" />][twitter]
+[<img align="left" alt="webnoob | Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />][instagram]
+[<img align="left" alt="webnoob | BuyMeACoffee" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@3.3.0/icons/buymeacoffee.svg" />][buymeacoffee]
+
+<br>
+<br>
 
 ## Project setup
 ```
@@ -25,5 +54,13 @@ npm run test
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## License
+
+[MIT](LICENSE)
+
+
+[website]: https://webnoob.dev
+[twitter]: https://twitter.com/webnoobcodes
+[youtube]: https://youtube.com/webnoob
+[instagram]: https://instagram.com/webnoobcodes
+[buymeacoffee]: https://www.buymeacoffee.com/webnoob
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="{'theme-dark': nightMode}">
+    <div class="container">
+      <nav>
+        <i class="fas fa-sun icon-sun" v-if="nightMode" @click="nightMode = !nightMode"></i>
+        <i class="fas fa-moon icon-moon" v-else @click="nightMode = !nightMode"></i>
+      </nav>
+      <article>
+        <img src="./assets/test-image.jpg" alt="">
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        </p>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data: () => {
+      return {
+        nightMode: false
+      }
+    },
+    watch: {
+      nightMode: function() {
+        localStorage.setItem("nightMode", JSON.stringify(this.nightMode));
+      }
+    },
+    created() {
+      this.nightMode = JSON.parse(localStorage.getItem("nightMode"));
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://use.fontawesome.com/releases/v5.8.1/css/all.css');
+
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    width: calc(100% - 20px);
+    min-height: 100vh;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all .5s ease-in-out;
+
+    color: #333;
+    background-color: #efefef;
+
+    .container {
+      max-width: 550px;
+
+      nav {
+        position: relative;
+        height: 50px;
+
+        i {
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          font-size: 1.6rem;
+          cursor: pointer;
+        }
+
+        .icon-sun {
+          color: #FCDA5F;
+        }
+        .icon-moon {
+          color: #041B37;
+        }
+      }
+
+      article {
+        position: relative;
+        padding: 10px;
+        background-color: #fff;
+        border: 1px solid #ccc;
+
+        img {
+          max-width: 100%;
+          height: auto;
+        }
+
+        p {
+          padding: 10px;
+
+          &::first-letter {
+              float: left;
+              font-size: 4rem;
+              line-height: .8;
+              padding: 0 10px 0 0;
+          }
+        }
+      }
+    }
+
+    &.theme-dark {
+      color: #efefef;
+      background-color: #333;
+
+      article {
+        background-color: #222;
+        border: 1px solid #555;
+      }
+    }
+  }
 </style>
